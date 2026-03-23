12e8cbe202216e745a5be04c5f68aa49c776d61d
Ajish18 <ajishiyappan1@gmail.com>
2026-03-23 15:58:43 +0530

    Designed Employee and Travel Request DocTypes

---
+++ etems/expense_management/doctype/employee_details/employee_details.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2026, ajish and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Employee Details", {
+	setup(frm){
+        frm.set_query("reporting_manager", function(){
+            return{
+                filters:{
+                    role: "Reporting Manager"
+                }
+            };
+        });
+    },
+});
---
+++ etems/expense_management/doctype/policy_settings/policy_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, ajish and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PolicySettings(Document):
+	pass
