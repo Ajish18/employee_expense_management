9cc395e9b41367412b4f5a98487db375dfd090f4
Ajish18 <ajishiyappan1@gmail.com>
2026-03-24 13:18:30 +0530

    fix: pre-commit issues resolved

--- etems/expense_management/doctype/employee_details/employee_details.js
+++ etems/expense_management/doctype/employee_details/employee_details.js
@@ -2,13 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Employee Details", {
-	setup(frm){
-        frm.set_query("reporting_manager", function(){
-            return{
-                filters:{
-                    role: "Reporting Manager"
-                }
-            };
-        });
-    },
+	setup(frm) {
+		frm.set_query("reporting_manager", function () {
+			return {
+				filters: {
+					role: "Reporting Manager",
+				},
+			};
+		});
+	},
 });
--- etems/expense_management/doctype/travel_request/travel_request.py
+++ etems/expense_management/doctype/travel_request/travel_request.py
@@ -6,27 +6,26 @@ from frappe.model.document import Document
 
 
 class TravelRequest(Document):
-    
-    def before_save(self):
-        if self.is_new():
-            self.status="Pending Manager Approval"
+	def before_save(self):
+		if self.is_new():
+			self.status = "Pending Manager Approval"
 
-    def on_cancel(self):
-        self.status="Rejected"
+	def on_cancel(self):
+		self.status = "Rejected"
 
 
 @frappe.whitelist()
 def manager_approval(name):
-    frappe.db.set_value("Travel Request", name, "status", "Pending Finance Verification")
-    doc=frappe.get_doc("Travel Request", name)
-    if doc.email:
-        frappe.sendmail(
-            recipients=[doc.email],
-            subject="Travel Request Rejected",
-            message=f"""
+	frappe.db.set_value("Travel Request", name, "status", "Pending Finance Verification")
+	doc = frappe.get_doc("Travel Request", name)
+	if doc.email:
+		frappe.sendmail(
+			recipients=[doc.email],
+			subject="Travel Request Rejected",
+			message=f"""
             <p>Dear {doc.employee_name},</p>
             <p>
-            Your Travel Request <b>{doc.name}</b> has been sent for 
+            Your Travel Request <b>{doc.name}</b> has been sent for
             <b>Finance Verification</b> by your Reporting Manager.
             </p>
             <p>
@@ -37,17 +36,18 @@ def manager_approval(name):
             Reporting Manager,<br>
             <b>{doc.reporting_manager_name}</b>
             </p>
-                        """
-        )
+                        """,
+		)
+
 
 @frappe.whitelist()
 def manager_reject(name):
-    frappe.db.set_value("Travel Request", name, "status", "Rejected")
-    doc=frappe.get_doc("Travel Request", name)
-    frappe.sendmail(
-            recipients=[doc.email],
-            subject="Travel Request Rejected",
-            message=f"""
+	frappe.db.set_value("Travel Request", name, "status", "Rejected")
+	doc = frappe.get_doc("Travel Request", name)
+	frappe.sendmail(
+		recipients=[doc.email],
+		subject="Travel Request Rejected",
+		message=f"""
             <p>Dear {doc.employee_name},</p>
             <p>
             Your Travel Request <b>{doc.name}</b> has been Rejected by your Reporting Manager.
@@ -60,43 +60,45 @@ def manager_reject(name):
             Reporting Manager,<br>
             <b>{doc.reporting_manager_name}</b>
             </p>
-            """
-        )
+            """,
+	)
+
 
 @frappe.whitelist()
-def finance_manager_reject(name,reason):
-    frappe.db.set_value("Travel Request", name, "status", "Cancelled")
-    doc=frappe.get_doc("Travel Request", name)
-    doc.cancel_reason=reason
-    doc.save()
-    doc.submit()
-    doc.cancel()
-    frappe.sendmail(
-            recipients=[doc.email],
-            subject="Travel Request Rejected",
-            message=f"""
+def finance_manager_reject(name, reason):
+	frappe.db.set_value("Travel Request", name, "status", "Cancelled")
+	doc = frappe.get_doc("Travel Request", name)
+	doc.cancel_reason = reason
+	doc.save()
+	doc.submit()
+	doc.cancel()
+	frappe.sendmail(
+		recipients=[doc.email],
+		subject="Travel Request Rejected",
+		message=f"""
             Dear {doc.employee_name},
             Your Travel Request <b>{doc.name}</b> has been rejected/cancelled by the Finance Manager.
             <b>Reason:</b> {reason}
             Please contact the Finance team for more details.
             Regards,<br>
             Finance Team
-            """
-        )
+            """,
+	)
+
 
 @frappe.whitelist()
 def finance_manager_approval(name):
-    frappe.db.set_value("Travel Request", name, "status", "Approved")
-    doc=frappe.get_doc("Travel Request", name)
-    doc.submit()
-    if doc.email:
-        frappe.sendmail(
-            recipients=[doc.email],
-            subject="Travel Request Rejected",
-            message=f"""
+	frappe.db.set_value("Travel Request", name, "status", "Approved")
+	doc = frappe.get_doc("Travel Request", name)
+	doc.submit()
+	if doc.email:
+		frappe.sendmail(
+			recipients=[doc.email],
+			subject="Travel Request Rejected",
+			message=f"""
             <p>Dear {doc.employee_name},</p>
             <p>
-            Your Travel Request <b>{doc.name}</b> has been approved by the Finance Manager. 
+            Your Travel Request <b>{doc.name}</b> has been approved by the Finance Manager.
             </p>
             <p>
             Please contact your manager for more details.
@@ -105,19 +107,19 @@ def finance_manager_approval(name):
             Regards,<br>
             Finace Team,<br>
             </p>
-            """
-        )
+            """,
+		)
 
 
 def travel_request_query(user):
-    roles = frappe.get_roles(user)
-    if "System Manager" in roles or user == "Administrator":
-        return
-    if "Reporting Manager" in roles:
-        return f"""
+	roles = frappe.get_roles(user)
+	if "System Manager" in roles or user == "Administrator":
+		return
+	if "Reporting Manager" in roles:
+		return f"""
         `tabTravel Request`.reporting_manager='{user}'
         """
-    if "Finance Manager" in roles:
-        return """
+	if "Finance Manager" in roles:
+		return """
         `tabTravel Request`.status IN ('Pending Finance Verification', 'Cancelled') """
-    return ""
+	return ""
